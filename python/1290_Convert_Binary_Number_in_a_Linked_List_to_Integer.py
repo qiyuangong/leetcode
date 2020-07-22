cb863d7fd2efebdf843fe4a5f38df012926e78b4
Aayush Srivastava <aayu.2312@gmail.com>
2020-07-23 01:19:23 +0530

    Update 1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.py

--- python/1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.py
+++ python/1290_Convert_Binary_Number_in_a_Linked_List_to_Integer.py
@@ -1,6 +1,3 @@
-#Question: https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
-#Author: Aayush Srivastava
-
 '''
 Given head which is a reference node to a singly-linked list. 
 The value of each node in the linked list is either 0 or 1. 
