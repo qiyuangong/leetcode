8bd8f4591906d6a2e7e094d3b0c20a95991f4565
ROMEEZHOU <yz7258@nyu.edu>
2023-03-20 07:14:28 +0800

    add bitwise solution for 007 reverse integer

--- python/007_Reverse_Integer.py
+++ python/007_Reverse_Integer.py
@@ -1,17 +1,24 @@
 class Solution:
     def reverse(self, x):
         # https://leetcode.com/problems/reverse-integer/
-        flag = True if x < 0 else False
-        if flag:
-            x = -x
-        x = str(x)[::-1]
+#        flag = True if x < 0 else False
+#       if flag:
+#           x = -x
+#       x = str(x)[::-1]
 
-        if flag:
-            x = "-" + x
+#       if flag:
+#           x = "-" + x
 
-        value = 2 ** 31
-        x = int(x)
-        if -value <= x < value:
-            return x
-        return 0
-    
+#       value = 2 ** 31
+#       x = int(x)
+#       if -value <= x < value:
+#           return x
+#       return 0
+
+        res = 0
+        for i in range(32):  
+            res = res << 1
+            bit = x % 2
+            res += bit
+            x = x >> 1
+        return res
